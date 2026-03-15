069ff86f27ce00da60886971d649d9ebcb8e2605
Ajmallpk <ajuaamal@gmail.com>
2026-03-15 17:30:59 +0530

    feat:permission middleware implemented

---
+++ backend/apps/accounts/middleware.py
@@ -0,0 +1,19 @@
+from django.http import JsonResponse
+
+
+
+class BlockSuspendedUserMiddleware:
+    def __init__(self,get_response):
+        self.get_response = get_response
+        
+    def __call__(self, request):
+        user = request.user
+        
+        if user.is_authenticated:
+            if hasattr(user,"status") and user.status == "SUSPENDED":
+                return JsonResponse(
+                    {"error":"your account has been suspended"},
+                    status = 403
+                )
+        return self.get_response(request)
+        
